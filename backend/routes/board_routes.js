bba951582c036814d15cf588ab0817ed94ec3b3e
Ragnhild Aalvik <raa009@student.uib.no>
2018-01-21 01:08:49 +0100

    Done making CRUD api and database

--- backend/routes/board_routes.js
+++ backend/routes/board_routes.js
@@ -1,4 +1,43 @@
+var ObjectID = require('mongodb').ObjectID;
+
 module.exports = function (app, db) {
+    app.get('/results/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id) };
+        db.collection('results').findOne(details, (err, item) => {
+            if (err) {
+                res.send({ 'error': 'An error has occurred' });
+            } else {
+                res.send(item);
+            }
+        });
+    });
+
+    app.delete('/results/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id) };
+        db.collection('results').remove(details, (err, item) => {
+            if (err) {
+                res.send({ 'error': 'An error has occurred' });
+            } else {
+                res.send('Note ' + id + ' deleted!');
+            }
+        });
+    })
+
+    app.put('/results/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id) };
+        const newResult = { name: req.body.name, score: req.body.score };
+        db.collection('results').update(details, newResult, (err, result) => {
+            if (err) {
+                res.send({ 'error': 'An error has occurred' });
+            } else {
+                res.send(newResult);
+            }
+        });
+    });
+
     app.post('/results', (req, res) => {
         const result = { name: req.body.name, score: req.body.score };
         db.collection('results').insert(result, (err, result) => {
@@ -7,6 +46,6 @@ module.exports = function (app, db) {
             } else {
                 res.send(result.ops[0]);
             }
-        })
+        });
     });
 };
